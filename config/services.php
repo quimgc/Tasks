e4437d2eedff301ce2ca3f4623b8b5048d3e55f6
quimgc <quimgonzalez@iesebre.com>
2017-12-19 17:29:12 +0100

    Sync

--- config/services.php
+++ config/services.php
@@ -43,6 +43,12 @@ return [
     */
     #llum_services
 
+    'google' => [
+        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
+        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
+        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
+    ],
+
     'facebook' => [
         'client_id' => env('FACEBOOK_OAUTH_APP_ID'),
         'client_secret' => env('FACEBOOK_OAUTH_APP_SECRET'),
--- vue/Tasks/config/index.js
+++ vue/Tasks/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-tasks',
 
     /**
      * Source Maps
