c7007bec3eaf94de62824997fa161ab8a64b2ff4
quimgc <quimgonzalez@iesebre.com>
2017-12-29 16:11:14 +0100

    Sync

--- config/services.php
+++ config/services.php
@@ -49,11 +49,6 @@ return [
         'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
     ],
 
-    'twitter' => [
-        'client_id' => env('TWITTER_OAUTH_APP_ID'),
-        'client_secret' => env('TWITTER_OAUTH_APP_SECRET'),
-        'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
-    ],
 
     'google' => [
         'client_id' => env('GOOGLE_OAUTH_APP_ID'),
@@ -61,29 +56,6 @@ return [
         'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
     ],
 
-    'google' => [
-        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
-        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
-        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
-    ],
-
-    'google' => [
-        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
-        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
-        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
-    ],
-
-    'google' => [
-        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
-        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
-        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
-    ],
-
-    'google' => [
-        'client_id' => env('GOOGLE_OAUTH_APP_ID'),
-        'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
-        'redirect' => env('GOOGLE_OAUTH_APP_REDIRECT_URL'),
-    ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_OAUTH_APP_ID'),
@@ -91,35 +63,35 @@ return [
         'redirect' => env('FACEBOOK_OAUTH_APP_REDIRECT_URL'),
     ],
 
-    'facebook' => [
-        'client_id' => env('FACEBOOK_OAUTH_APP_ID'),
-        'client_secret' => env('FACEBOOK_OAUTH_APP_SECRET'),
-        'redirect' => env('FACEBOOK_OAUTH_APP_REDIRECT_URL'),
-    ],
+//    'facebook' => [
+//        'client_id' => env('FACEBOOK_OAUTH_APP_ID'),
+//        'client_secret' => env('FACEBOOK_OAUTH_APP_SECRET'),
+//        'redirect' => env('FACEBOOK_OAUTH_APP_REDIRECT_URL'),
+//    ],
 
     'github' => [
         'client_id' => env('GITHUB_OAUTH_APP_ID'),
         'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
         'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
     ],
-
-    'github' => [
-        'client_id' => env('GITHUB_OAUTH_APP_ID'),
-        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
-        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
-    ],
-
-    'github' => [
-        'client_id' => env('GITHUB_OAUTH_APP_ID'),
-        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
-        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
-    ],
-
-    'github' => [
-        'client_id' => env('GITHUB_OAUTH_APP_ID'),
-        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
-        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
-    ],
+//
+//    'github' => [
+//        'client_id' => env('GITHUB_OAUTH_APP_ID'),
+//        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
+//        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
+//    ],
+//
+//    'github' => [
+//        'client_id' => env('GITHUB_OAUTH_APP_ID'),
+//        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
+//        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
+//    ],
+//
+//    'github' => [
+//        'client_id' => env('GITHUB_OAUTH_APP_ID'),
+//        'client_secret' => env('GITHUB_OAUTH_APP_SECRET'),
+//        'redirect' => env('GITHUB_OAUTH_APP_REDIRECT_URL'),
+//    ],
 
 
     /*
