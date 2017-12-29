150d8490b1d0062f4b312a423a5cdad3cbaad2bd
quimgc <quimgonzalez@iesebre.com>
2017-12-29 15:46:32 +0100

    Sync

--- config/services.php
+++ config/services.php
@@ -43,6 +43,18 @@ return [
     */
     #llum_services
 
+    'twitter' => [
+        'client_id' => env('TWITTER_OAUTH_APP_ID'),
+        'client_secret' => env('TWITTER_OAUTH_APP_SECRET'),
+        'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
+    ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_OAUTH_APP_ID'),
+        'client_secret' => env('TWITTER_OAUTH_APP_SECRET'),
+        'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
+    ],
+
     'google' => [
         'client_id' => env('GOOGLE_OAUTH_APP_ID'),
         'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
