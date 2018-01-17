1a11e1be1cbf0d9a7da904bca35aff2250938e6c
quimgc <quimgonzalez@iesebre.com>
2018-01-17 21:53:59 +0100

    Sync

--- vue/vue-tasks-custom-ui/src/App.vue
+++ vue/vue-tasks-custom-ui/src/App.vue
@@ -2,6 +2,21 @@
 
   <body>
       <div class="container">
+        <header>
+    <b>Admilite Layout</b>
+        </header>
+
+          <nav>
+            <!--<i class="da fa-address-card"></i>-->
+            <a href=""><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg></a>
+            <ul>
+              <li><a href="">Notifications</a></li>
+              <li><a href="">Tasks</a></li>
+              <li><a href="">Logged User</a></li>
+              <li><a href="">Preferences</a></li>
+            </ul>
+
+          </nav>
 
         <main>
 
@@ -15,11 +30,7 @@
 
         <footer>FOOTER</footer>
 
-        <header>
 
-          <i class="da fa-address-card"></i>
-          <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </header>
       </div>
 
   </body>
@@ -47,6 +58,12 @@ export default {
     font-weight: 500;
     font-size: 14px;
     color: #333;
+  }
+
+  a {
+
+    text-decoration: none;
+    color: inherit;
   }
 
   .container {
@@ -77,7 +94,6 @@ export default {
     background-color: #ecf0f5;
     grid-row: 2;
     grid-column: 2;
-
   }
 
   header {
@@ -101,8 +117,7 @@ export default {
 
     background-color: #ffffff;
     grid-row: 3;
-    grid-column-start: 1;
-    grid-column-end: 4;
+    grid-column: 2/4;
   }
 
   section {
@@ -112,7 +127,7 @@ export default {
   }
 
   .left_sidebar {
-    grid-row: 2;
+    grid-row: 2/4;
     grid-column: 1;
     background-color: #222d33;
 
@@ -122,7 +137,36 @@ export default {
 
     grid-row: 2;
     grid-column: 3;
-    background-color: #ecf0f5;
+    background-color: #222d33;
+
+  }
+
+  ul, li {
+
+    list-style: none;
+
+  }
+
+  nav {
+
+    grid-row: 1;
+    grid-column: 2/4;
+    background-color: #3c8dbb;
+    color: #fff;
+
+  }
+
+  nav ul {
+
+    display: flex;
+    /*margin-left: auto;*/
+    margin: 0px 0px 0px auto;
+
+  }
+
+  nav ul li {
+    /*display: flex;*/
+    border: 1px solid black;
 
   }
 
