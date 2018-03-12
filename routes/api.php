c6f1db2c6eb792f2e57223785fe4df430373b132
quimgc <quimgonzalez@iesebre.com>
2018-03-12 17:50:40 +0100

    Sync

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/proxy/oauth/token', 'PasswordGrantProxyController@issueToken');
+});
+
+
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
     //    Route::resource('task', 'TasksController');
 
